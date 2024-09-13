85563456fe89649e5455b84de014e42dd68c136a
723kb <pp5_vb.swd723@icloud.com>
2024-09-13 18:14:26 +0900

    Logo.jsxのパス指定修正

--- public/build/assets/SecondaryButton-DHbjL2BT.js
+++
@@ -1,3 +0,0 @@
-import{j as s}from"./app-BgAMWFci.js";function i({type:o="button",className:r="",disabled:t,children:e,...n}){return s.jsx("button",{...n,type:o,className:`inline-flex items-center px-4 py-2 border rounded-md font-semibold text-xs uppercase tracking-widest shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150 
-            ${t&&"opacity-25"}
-            ${r||"bg-gray-200 border-gray-300 text-gray-700 hover:bg-gray-300 focus:bg-gray-300 active:bg-gray-400 focus:ring-gray-500"}`,disabled:t,children:e})}export{i as S};
